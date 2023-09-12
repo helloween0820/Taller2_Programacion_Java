27ec23129313c4f8f42316b1d806b8a436e7243c
helloween0820 <81496949+helloween0820@users.noreply.github.com>
2023-09-11 20:56:53 -0500

    Add files via upload

---
+++ Mediana/src/main/java/com/mycompany/mediana/Mediana.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mediana;
+
+/**
+ *
+ * @author sebastiancaicedo
+ */
+public class Mediana {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
